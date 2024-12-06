a33452f7a90e378382ae04be4705e230e37b66e2
MrManGuy <rinicholas99@gmail.com>
2024-12-06 10:20:41 -0600

    Properly prompting choices

--- client/src/components/content/Characters.jsx
+++ client/src/components/content/Characters.jsx
@@ -3,6 +3,10 @@ import { Container, Row, Col, Button, Form, InputGroup } from "react-bootstrap"
 
 import CharacterCard from "../structural/CharacterCard"
 
+import raceFeatures from '../../jsons/races.json';
+import classFeatures from '../../jsons/classes.json';
+import weapons from '../../jsons/weapons.json';
+
 let characterList = [{
     "id": 1,
     "name": "Char 1"
@@ -22,141 +26,7 @@ let characterList = [{
 
 const classList = ["Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
 const abilities = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
-
-const classFeatures = {
-    "Artificer": {
-        'HitDie': '1d8',
-        'Proficiencies': {
-            'Armor': ['Light', 'Medium', 'Shields'],
-            'Weapons': ['Simple'],
-            'Tools': ["Thieves' tools", "Tinker's tools"],
-            'SavingThrows': ['Constitution', 'Intelligence'],
-            'SkillPool': ['Arcana', 'History', 'Investigation', 'Medicine', 'Nature', 'Perception', 'Sleight of Hand']
-        },
-        "SpellSlots": [2, 2, 0, 0, 0, 0],
-    },
-    "Barbarian": {
-        'HitDie': '1d12',
-        'Proficiencies': {
-            'Armor': ['Light', 'Medium', 'Shields'],
-            'Weapons': ['Simple', 'Martial'],
-            'Tools': [],
-            'SavingThrows': ['Strength', 'Constitution'],
-            'SkillPool': ['Animal Handling', 'Athletics', 'Intimidation', 'Nature', 'Perception', 'Survival']
-        },
-        "SpellSlots": [0, 0, 0, 0, 0, 0],
-    }
-}
-
 const raceList = ["Aasimar", "Dragonborn", "Dwarf", "Elf", "Gnome", "Goliath", "Halfling", "Human", "Tiefling"]
-const raceFeatures = {
-    "Aasimar": {
-        "AbilityScoreIncrease": ["2,1","1,1,1"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Medium", "Small"],
-        "Speed": 30,
-        "Darkvision": true,
-        "Resistances": ["Necrotic", "Radiant"],
-        "Languages": ["Common", "Other"]
-        //TODO: Healing Hands, Light Berard, & Celesital Revelation
-    },
-    "Dragonborn": {
-        "AbilityScoreIncrease": ["Strength", "Strength", "Charisma"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Medium"],
-        "Speed": 30,
-        "Darkvision": false,
-        "Resistances": [],
-        "Languages": ["Common", "Draconic"],
-        "DragonColor": ["Black", "Blue", "Brass", "Bronze", "Copper", "Gold", "Green", "Red", "Silver", "White"]
-        //TODO: Breath weapon, associated damage resistance
-    },
-    "Dwarf": {
-        "AbilityScoreIncrease": ["Constitution", "Constitution"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Medium"],
-        "Speed": 25,
-        "Darkvision": true,
-        "Resistances": ["Poison"],
-        "Proficiencies": {
-            "SavingThrows": ["Poison"],
-            "Weapons": ["Battleaxe", "Handaxe", "Light hammer", "Warmhammer"],
-            "Tools": ["smith's tools", "brewer's supplies", "mason's tools"] //TODO: Choose 1
-        },
-        "Languages": ["Common", "Dwarvish"]
-        //TODO: Subraces, stonecunning
-    },
-    "Elf":{
-        "AbilityScoreIncrease": ["Dexterity", "Dexterity"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Medium"],
-        "Speed": 30,
-        "Darkvision": true,
-        "Proficiencies": {
-            "SavingThrows": ["Charmed"],
-            "Skills": ["Perception"]
-        },
-        "Languages": ["Common", "Elvish"]
-        //TODO: Magic cannot put you to sleep, trance, subraces
-    }, 
-    "Gnome": {
-        "AbilityScoreIncrease": ["Intelligence", "Intelligence"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Small"],
-        "Speed": 25,
-        "Darkvision": true,
-        "Proficiencies": {
-            "SavingThrows": ["Intelligence", "Wisdom", "Charisma"],
-            "Skills": ["Perception"]
-        },
-        "Languages": ["Common", "Gnomish"]
-        //TODO: Subraces
-    }, 
-    "Goliath": {
-        "AbilityScoreIncrease": ["Strength", "Strength", "Constitution"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Medium"],
-        "Speed": 30,
-        "Darkvision": false,
-        "Resistances": ["Cold"],
-        "Proficiencies": {
-            "Skills": ["Athletics"]
-        },
-        "Languages": ["Common", "Giant"]
-        //TODO: Powerful Build, Stone's endurance
-    }, 
-    "Halfling": {
-        "AbilityScoreIncrease": ["Dexterity", "Dexterity"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Small"],
-        "Speed": 25,
-        "Darkvision": false,
-        "Proficiencies": {
-            "SavingThrows": ["Frightened"]
-        },
-        "Languages": ["Common", "Halfling"]
-        //TODO: Lucky, Nimble, subraces
-    }, 
-    "Human": {
-        "AbilityScoreIncrease": ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Medium"],
-        "Speed": 30,
-        "Darkvision": false,
-        "Languages": ["Common", "Other"]
-        //TODO: Variant
-    },
-    "Tiefling": {
-        "AbilityScoreIncrease": ["Charisma", "Charisma"],
-        "CreatureType": "Humanoid",
-        "SizeOptions": ["Medium"],
-        "Speed": 30,
-        "Darkvision": true,
-        "Resistances": ["Fire"],
-        "Languages": ["Common", "Infernal"]
-        //TODO: Bloodlines
-    }
-}
 
 const Characters = () => {
     const [activeCharacter, setActiveCharacter] = useState('')
@@ -188,7 +58,7 @@ const Characters = () => {
     return <Fragment>
         <Container fluid className="page_body">
             {
-            creatingCharacter === 0 ? <>
+            creatingCharacter === 0 ? <Fragment>
             <Button onClick={e => setCreatingCharacter(1)}>Create New Character</Button>
             <Row xs={1} sm={1} md={2} lg={3} xl={4} >
                 {
@@ -198,8 +68,8 @@ const Characters = () => {
                     </Col>
                 )
                 }
-            </Row></>
-            :<>
+            </Row></Fragment>
+            :<Fragment>
             <Button variant='danger' onClick={e => setCreatingCharacter(0)}>Cancel Character Creation</Button>
 
             <Form>
@@ -227,7 +97,7 @@ const Characters = () => {
                     <Form.Group as={Col} lg={3} controlId="characterCreation.characterRace">
                         <Form.Label>Race</Form.Label>
                         <Form.Select>
-                            {raceList.map(raceName => <option>{raceName}</option>)}
+                            {raceList.map(raceName => <option key={raceName}>{raceName}</option>)}
                         </Form.Select>
                     </Form.Group>
 
@@ -235,10 +105,10 @@ const Characters = () => {
                         <Form.Label>Alignment</Form.Label>
                         <InputGroup className="mb-3">
                             <Form.Select>
-                                {["Lawful", "Neutral", "Chaotic"].map(first => <option>{first}</option>)}
+                                {["Lawful", "Neutral", "Chaotic"].map(first => <option key={first}>{first}</option>)}
                             </Form.Select>
                             <Form.Select>
-                                {["Good", "Neutral", "Evil"].map(last => <option>{last}</option>)}
+                                {["Good", "Neutral", "Evil"].map(last => <option key={last}>{last}</option>)}
                             </Form.Select>
                         </InputGroup>
                     </Form.Group>
@@ -251,11 +121,11 @@ const Characters = () => {
                     <h4 className="section_subheader">If you want to use values outside of the [3, 18] range, you will have to edit your character after creation</h4>
                 </Form.Label>
                 <Row className="mb-3">
-                    {abilities.map(attribute => 
-                        <Form.Group as={Col} lg={2} controlId={`characterCreation.character${attribute}`}>
+                    {abilities.map(ability => 
+                        <Form.Group key={ability} as={Col} lg={2} controlId={`characterCreation.character${ability}`}>
                             <Form.Label>
-                                {attribute} {
-                                (classFeatures[selectedClass] != null && classFeatures[selectedClass].Proficiencies.SavingThrows.includes(attribute)) 
+                                {ability} {
+                                (classFeatures[selectedClass] != null && classFeatures[selectedClass]["Proficiencies"]["SavingThrows"].includes(ability)) 
                                 ? <strong>- Saving Throw</strong>
                                 : null}
                             </Form.Label>
@@ -269,25 +139,84 @@ const Characters = () => {
                 <Form.Label>
                     <h3 className="section_header">Class Selections</h3>
                 </Form.Label>
-                <Row className="mb-3">
-                    {classFeatures[selectedClass] !== undefined ?
-                        <>
-                        <p><strong>Hit Die:</strong> {classFeatures[selectedClass].HitDie}</p>
-                        <ul>
-                            {Object.keys(classFeatures[selectedClass].Proficiencies).map(key => (
-                                <li key={key}>
-                                    <strong>{key}:</strong> {classFeatures[selectedClass].Proficiencies[key].length !== 0 ? classFeatures[selectedClass].Proficiencies[key].join(', ') : "None"}
-                                </li>
-                            ))}
-                        </ul>
-                        </>
+                    {classFeatures["MainClasses"][selectedClass] !== undefined ?
+                        <Fragment>
+                        <Row className="mb-3">
+                        <p><strong>Hit Die:</strong> {classFeatures["MainClasses"][selectedClass]["Hit Die"]}</p>
+                        {Object.keys(classFeatures["MainClasses"][selectedClass]["Proficiencies"]).map(key => {
+                            //The proficiency is a choice
+                            if(Array.isArray(classFeatures["MainClasses"][selectedClass]["Proficiencies"][key][0])){
+                                let choiceGroups = classFeatures["MainClasses"][selectedClass]["Proficiencies"][key]
+                                return <Fragment key={key.replaceAll(" ", "")}>
+                                {choiceGroups.map(group => {
+                                    return <Form.Group key={group[0]} as={Col} lg={3} controlId={`characterCreation.character${key.replaceAll(" ", "") + group[0]}`}>
+                                            <Form.Label>{key}</Form.Label>
+                                            <Form.Select>
+                                                {group.slice(1).map(choice => <option key={choice}>{`${choice}`}</option>)}
+                                            </Form.Select>
+                                        </Form.Group>
+                                })}
+                                </Fragment>
+                            }
+                            //The proficiency is forced
+                            return <p key={key.replaceAll(" ", "")}>
+                                <strong>{key}:</strong> {classFeatures["MainClasses"][selectedClass]["Proficiencies"][key].length !== 0 ? classFeatures["MainClasses"][selectedClass]["Proficiencies"][key].join(', ') : "None"}
+                            </p>
+                        })}
+                        </Row>
+                        <Row className="mb-3">
+                        {Object.keys(classFeatures["MainClasses"][selectedClass]["Starting Equipment"]).map(category => {
+                            if(Array.isArray(classFeatures["MainClasses"][selectedClass]["Starting Equipment"][category])){
+                                //The category is an array, which means you either get more than one item or you get a choice
+                                return <Fragment key={category.replaceAll(" ", "")}>
+                                    {classFeatures["MainClasses"][selectedClass]["Starting Equipment"][category].map(element => {
+                                        if(Array.isArray(element)){
+                                            //The element is an array, this item is a choice
+                                            if(element[1] === "Any"){
+                                                //Chose any of element[1]
+                                                if(element[2].includes("Weapons")){
+                                                    let properties = element[2].split(" ").filter(property => property !== "Weapons")
+                                                    let propertyMatchedWeapons = Object.keys(weapons).filter(weapon => {
+                                                        for(let property of properties){
+                                                            if(!weapons[weapon].Attributes.includes(property)){
+                                                                return false
+                                                            }
+                                                        }
+                                                        return true
+                                                    })
+                                                    return <Form.Group key={element[0]} as={Col} lg={3} controlId={`characterCreation.character${category.replaceAll(" ", "") + element[0]}`}>
+                                                            <Form.Label>{category}</Form.Label>
+                                                            <Form.Select>
+                                                            {propertyMatchedWeapons.map(weapon => <option key={weapon}>{`${weapon} - ${weapons[weapon]["Damage"]} ${weapons[weapon]["Damage Type"]}`}</option>)}
+                                                            </Form.Select>
+                                                        </Form.Group>
+                                                }
+                                                return <p key={"BROKEN"}>{element[2]} Needs to be implemented</p>
+                                            }else{
+                                                //Chose one of any of the elements in element
+                                                return <Form.Group as={Col} lg={3} controlId={`characterCreation.character${category.replaceAll(" ", "")}`}>
+                                                    <Form.Label>{category}</Form.Label>
+                                                    <Form.Select>
+                                                        {element.map(choice => <option key={choice}>{choice}</option>)}
+                                                    </Form.Select>
+                                                </Form.Group>
+                                            }
+                                        }
+                                        return <Fragment key={element}></Fragment>
+                                    })}
+                                </Fragment>
+                            }
+
+                            return <p key={category.replaceAll(" ", "")}>{classFeatures["MainClasses"][selectedClass]["Starting Equipment"][category]}</p>
+                        })}
+                        </Row>
+                        </Fragment>
                         :
                         <p>This class has not been fleshed out yet</p>
                     }
-                </Row>
             </Form>
 
-            </>
+            </Fragment>
             }
         </Container>
     </Fragment>
